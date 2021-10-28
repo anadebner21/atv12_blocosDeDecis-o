83a9447da62c30c8395f57c25288869a9a2123bb
Ana Paula <anapdebner21@gmail.com>
2021-10-27 22:51:01 -0300

    Add o alert

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ function validacao(){
     console.log(senha);
     if(nome == 'aluno' && senha == '123' || nome == 'professor' && senha == '456'){
         console.log('Login válido, prossiga!! =)');
+        alert('Parabéns seu login está correto!!');
     }
     else{
         console.log('Login inválido. TENTE NOVAMENTE =(');
+        alert('Inválido!! Tente Novamente!');
     }
 }
